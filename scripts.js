919a4a1869f395ea5ef86cdbe7db20737049af6c
MeadowsMustangs <33379325+MeadowsMustangs@users.noreply.github.com>
2017-12-09 10:43:08 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ function login() {
   
 
 
-   if ((username === 'hello' && password === 'coolpeeps101'|| username === 'hi' && password === 'dopebreh')){
+   if ((username === 'hello' && password === 'coolpeeps101') || (username === 'hi' && password === 'dopebreh')){
 
 
        document.getElementById('error').classList.add('hide');
