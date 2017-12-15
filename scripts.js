0724d70486547e8eb82f6dc61430f7bcd12492bb
iLikeDonut <33379308+iLikeDonut@users.noreply.github.com>
2017-12-15 15:41:17 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-
+"'
 function login() {
 
 
@@ -19,7 +19,7 @@ function login() {
        document.getElementById('error').classList.add('hide');
 
 
-       window.location.href = 'home.html
+       window.location.href = 'home.html'
 
 
    } else {
